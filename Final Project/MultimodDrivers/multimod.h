703155de2325d73455382eecdd7505e472c01ca2
Ethan Haengel <58488884+Haengele@users.noreply.github.com>
2025-08-18 23:38:17 -0400

    Archiving Microprocessors II Final Project

---
+++ Final Project/MultimodDrivers/multimod.h
@@ -0,0 +1,14 @@
+#ifndef MULTIMOD_H_
+#define MULTIMOD_H_
+
+#include "multimod_BMI160.h"
+#include "multimod_i2c.h"
+#include "multimod_joystick.h"
+#include "multimod_OPT3001.h"
+#include "multimod_PCA9956b.h"
+#include "multimod_uart.h"
+#include "multimod_ST7789.h"
+#include "multimod_spi.h"
+#include "multimod_buttons.h"
+
+#endif /* MULTIMOD_H_ */
